eecb1e9d026ffbacfbc2eab5281ad57abdc20b58
Danilo Gomes de Moura <danilogmoura@gmail.com>
2023-08-03 22:29:47 -0300

    Definindo os gráficos dos jogadores

--- bomber.asm
+++ bomber.asm
@@ -9,11 +9,24 @@
     seg.u Variables
     org $80
 
-JetXPox     byte            ; posição X do jogador 0
-JetYPox     byte            ; posição Y do jogador 0
+JetXPox         byte        ; posição X do jogador 0
+JetYPox         byte        ; posição Y do jogador 0
 
-BomberXPos  byte            ; posição X do jogador 1
-BomberYPos  byte            ; posição Y do jogador 1
+BomberXPos      byte        ; posição X do jogador 1
+BomberYPos      byte        ; posição Y do jogador 1
+
+JetSpritePtr    word        ; ponteiro para a sprite do jogador 0
+JetColorPtr     word        ; ponteiro para a cor do jogador 0
+
+BomberSpritePtr word        ; ponteiro para a sprite do jogador 1
+BomberColorPtr  word        ; ponteiro para a cor do jogador 1
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Constantes
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+JET_HEIGHT = 9              ; altura da sprite do jogador 0 (linhas na lookup table)
+BOMBER_HEIGHT = 9           ; altura da sprite do jogador 1 (linhas na lookup table)
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Iniciar nosso segmento de código na ROM
@@ -29,10 +42,41 @@ Reset:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     lda #10
     sta JetYPox             ; JetYPos = 10
-
     lda #60
     sta JetXPox             ; JetXPos = 60
-    
+
+    lda #83
+    sta BomberYPos          ; BomberYPos = 83
+    lda #54
+    sta BomberXPos          ; BomberXPos = 54
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Inicialização das variaveis referentes as sprites (lookup table)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    lda #<JetSprite         ; carrega o byte menos significativo do endereço da sprite do jogador 0
+    sta JetSpritePtr        ; salva o endereço da sprite do jogador 0
+
+    lda #>JetSprite         ; carrega o byte mais significativo do endereço da sprite do jogador 0
+    sta JetSpritePtr+1      ; salva o endereço da sprite do jogador 0
+
+    lda #<JetColor          ; carrega o byte menos significativo do endereço da cor do jogador 0
+    sta JetColorPtr         ; salva o endereço da cor do jogador 0
+
+    lda #>JetColor          ; carrega o byte mais significativo do endereço da cor do jogador 0
+    sta JetColorPtr+1       ; salva o endereço da cor do jogador 0
+
+    lda #<BomberSprite      ; carrega o byte menos significativo do endereço da sprite do jogador 1
+    sta BomberSpritePtr     ; salva o endereço da sprite do jogador 1
+
+    lda #>BomberSprite      ; carrega o byte mais significativo do endereço da sprite do jogador 1
+    sta BomberSpritePtr+1   ; salva o endereço da sprite do jogador 1
+
+    lda #<BomberColor       ; carrega o byte menos significativo do endereço da cor do jogador 1
+    sta BomberColorPtr      ; salva o endereço da cor do jogador 1
+
+    lda #>BomberColor       ; carrega o byte mais significativo do endereço da cor do jogador 1
+    sta BomberColorPtr+1    ; salva o endereço da cor do jogador 1
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; iniciar o loop de exibição principal e a renderização do quadro
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -101,6 +145,78 @@ Overscan:
 ;; Loop principal do jogo, volta para o inicio do loop de exibição principal
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     jmp StartFrame          ; vai para o proximo frame  
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Define o array de bytes para a sprite
+;; IMPORTANTE!!! 
+;; Devem estar sempre nos endereços finais da ROM
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+JetSprite:
+    .byte #%00000000        ;
+    .byte #%00010100        ;   # #
+    .byte #%01111111        ; #######
+    .byte #%00111110        ;  #####
+    .byte #%00011100        ;   ###
+    .byte #%00011100        ;   ###
+    .byte #%00001000        ;    #
+    .byte #%00001000        ;    #
+    .byte #%00001000        ;    #
+
+JetSpriteTurn:
+    .byte #%00000000        ;
+    .byte #%00001000        ;    #
+    .byte #%00111110        ;  #####
+    .byte #%00011100        ;   ###
+    .byte #%00011100        ;   ###
+    .byte #%00011100        ;   ###
+    .byte #%00001000        ;    #
+    .byte #%00001000        ;    #
+    .byte #%00001000        ;    #
+
+BomberSprite:
+    .byte #%00000000        ;
+    .byte #%00001000        ;    #
+    .byte #%00001000        ;    #
+    .byte #%00101010        ;  # # #
+    .byte #%00111110        ;  #####
+    .byte #%01111111        ; #######
+    .byte #%00101010        ;  # # #
+    .byte #%00001000        ;    #
+    .byte #%00011100        ;   ###
+
+JetColor:
+    .byte #$00
+    .byte #$FE
+    .byte #$0C
+    .byte #$0E
+    .byte #$0E
+    .byte #$04
+    .byte #$BA
+    .byte #$0E
+    .byte #$08
+
+JetColorTurn:
+    .byte #$00
+    .byte #$FE
+    .byte #$0C
+    .byte #$0E
+    .byte #$0E
+    .byte #$04
+    .byte #$0E
+    .byte #$0E
+    .byte #$08
+
+BomberColor:
+    .byte #$00
+    .byte #$32
+    .byte #$32
+    .byte #$0E
+    .byte #$40
+    .byte #$40
+    .byte #$40
+    .byte #$40
+    .byte #$40
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Completa a ROM com 4KB, exigência do 6502
